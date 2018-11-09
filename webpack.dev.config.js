54937b0d7901da011566bb86fafe54ae9c177863
Schreibmann <jewishreich@gmail.com>
2018-11-10 01:39:11 +0300

    fix dev config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,8 +4,12 @@ const baseConfig = require('./webpack.base.config');
 const devConfig = {
   mode: 'development',
   devServer: {
+    port: 3042,
+    historyApiFallback: true,
+    overlay: true,
+    open: true,
     hot: true,
-    contentBase: __dirname + '/dist'
+    contentBase: `${__dirname}/dist`,
   },
   devtool: 'source-map',
 };
